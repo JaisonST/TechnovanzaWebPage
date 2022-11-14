a983b7614464864b595b6ffc8a44812f2c465e03
JaisonST <jaisonmanu@gmail.com>
2022-11-14 15:15:08 +0400

    profile qr

---
+++ js/profile_script.js
@@ -0,0 +1,36 @@
+/*=============== SHOW PROFILE MODAL ===============*/
+const showProfileModal = (openButton, modalContent) =>{
+    const openBtn = document.getElementById(openButton),
+    modalContainer = document.getElementById(modalContent)
+    
+    if(openBtn && modalContainer){
+        openBtn.addEventListener('click', ()=>{
+            //set name 
+            var name = getCookie("user_name");
+            var name_element = document.getElementById("profile-modal-name");
+            name_element.innerHTML = `Hey ${name}!`; 
+            
+            //set qr code 
+            var email = getCookie("user_email");
+            const qr = document.getElementById('qrcode');
+            qr.innerHTML = '';
+            const qrcode = new QRCode('qrcode', {
+                text: email,
+                width: 500,
+                height: 500,
+            });
+
+            modalContainer.classList.add('show-profile_modal')
+        })
+    }
+}
+showProfileModal('open-profile-modal','modal-profile-container')
+
+/*=============== CLOSE PROFILE MODAL ===============*/
+const closeProfileBtn = document.querySelectorAll('.close-profile-modal')
+
+function closeProfileModal(){
+    const modalContainer = document.getElementById('modal-profile-container')
+    modalContainer.classList.remove('show-profile_modal')
+}
+closeProfileBtn.forEach(c => c.addEventListener('click', closeProfileModal))
